8212f35d5c5c1200f99460d6a9f5f3d32e875612
xiaobai <319331998@qq.com>
2023-11-14 19:05:31 +0800

    Nov 14

--- lab.cpp
+++ lab.cpp
@@ -1,18 +1,29 @@
-#include "./lab.h"
-class Student {
-    public:
-        char *get_name();
-        void set_name(char *);
+#include <iostream>
 
-    private:
-        char *name;
-};
+void swap(int &a, int &b) {
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
 
-char *Student::get_name() { return name; }
-void Student::set_name(char *str) { name = str; }
+void reverse(int *arry, int start, int end) {
+    for (int i = start; i < (start + end) / 2; i++) {
+        swap(arry[i], arry[end + start - i]);
+    }
+}
+
+void shift_left(int *arry, int size, int p) {
+    reverse(arry, 0, size - 1);
+    reverse(arry, 0, size - p - 1);
+    reverse(arry, size - p, size - 1);
+}
 
 int main() {
-    Student s = Student();
-    char name[] = "John";
-    s.set_name(name);
+    int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    shift_left(a, 10, 7);
+    for (int i = 0; i < 10; i++) {
+        std::cout << a[i] << ", ";
+    }
+    return 0;
 }
--- lab.py
+++ lab.py
@@ -1,18 +1,62 @@
-import asyncio
+from typing import Self
+from queue import Queue
 
 
-async def func(name):
-    for i in range(100):
-        print(f"first doing someting at {i} of {name}")
-        await asyncio.sleep(1)
-        print(f"second doing someting at {i} of {name}")
+class Node:
+    def __init__(self, val=-1, left: Self = None, right: Self = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def show(self):
+        if self.left:
+            self.left.show()
+        print(self.val)
+        if self.right:
+            self.right.show()
 
 
-async def main():
-    tasks = []
-    for i in range(100):
-        tasks.append(func(i))
-    await asyncio.gather(*tasks)
+class Tree:
+    def __init__(self, head: Self = None):
+        self.head = head
+
+    def show(self):
+        self.head.show()
+
+    def get_WPL(self):
+        WPL = 0
+        q = Queue()
+        q.put(self.head)
+        level = -1
+        while not q.empty():
+            level += 1
+            for i in range(q.qsize()):
+                item = q.get()
+                if not (item.left or item.right):
+                    WPL += item.val * level
+                else:
+                    if item.left:
+                        q.put(item.left)
+                    if item.right:
+                        q.put(item.right)
+        return WPL
 
 
-asyncio.run(main())
+def create_test_tree() -> Tree:
+    n1 = Node(1)
+    n2 = Node(2)
+    n3 = Node(3)
+    n4 = Node(4)
+    n1.left = n2
+    n1.right = n3
+    n3.right = n4
+    tree = Tree(n1)
+    return tree
+
+
+if __name__ == "__main__":
+    tree = create_test_tree()
+    tree.show()
+    print()
+    wpl = tree.get_WPL()
+    print(wpl)
